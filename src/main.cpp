4d147c2e2fddc85ea22e0005d9d064253fb8c2bc
Kristjan <kristjan.maysoln@gmail.com>
2020-12-20 17:35:59 +0100

    not working, old library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,108 @@
-#include <Arduino.h>
+ /*   NOTES:
+  *   - Magentometer I2C address: 0b0011110 oz. 0x1E
+  *   - Accelerometer I2C address: 0x19
+  *
+  */
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+
+bool state = HIGH;
+uint16_t pinLed = 14;
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  mag.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print("Sensor:       ");
+  Serial.println(sensor.name);
+  Serial.print("Driver Ver:   ");
+  Serial.println(sensor.version);
+  Serial.print("Unique ID:    ");
+  Serial.println(sensor.sensor_id);
+  Serial.print("Max Value:    ");
+  Serial.print(sensor.max_value);
+  Serial.println(" uT");
+  Serial.print("Min Value:    ");
+  Serial.print(sensor.min_value);
+  Serial.println(" uT");
+  Serial.print("Resolution:   ");
+  Serial.print(sensor.resolution);
+  Serial.println(" uT");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup(void)
+{
+  pinMode(pinLed, OUTPUT);
+
+  delay(500);
+  /*
+#ifndef ESP8266
+  while (!Serial)
+    ; // will pause Zero, Leonardo, etc until serial console opens
+#endif
+*/
+  Serial.begin(9600);
+  Serial.println("Magnetometer Test");
+  Serial.println("");
+
+  /* Enable auto-gain */
+  mag.enableAutoRange(false);
+  mag.setMagGain(LSM303_MAGGAIN_1_3);
+  mag.setMagRate(LSM303_MAGRATE_0_7);
+
+
+
+  /* Initialise the sensor */
+  while (!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    delay(2000);
+  }
+
+  /* Display some basic information on this sensor */
+  //displaySensorDetails();
+}
+
+void loop(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  mag.getEvent(&event);
+
+  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+  Serial.print("X: ");
+  Serial.print(event.magnetic.x);
+  Serial.print("  ");
+  Serial.print("Y: ");
+  Serial.print(event.magnetic.y);
+  Serial.print("  ");
+  Serial.print("Z: ");
+  Serial.print(event.magnetic.z);
+  Serial.print("  ");
+  Serial.println("uT");
+
+  /* Note: You can also get the raw (non unified values) for */
+  /* the last data sample as follows. The .getEvent call populates */
+  /* the raw values used below. */
+  // Serial.print("X Raw: "); Serial.print(mag.raw.x); Serial.print("  ");
+  // Serial.print("Y Raw: "); Serial.print(mag.raw.y); Serial.print("  ");
+  // Serial.print("Z Raw: "); Serial.print(mag.raw.z); Serial.println("");
+
+  /* Delay before the next sample */
+  
+  // heartbeat
+  if(state == HIGH) state = LOW;
+  else state = HIGH;
+  digitalWrite(pinLed, state);
+
+  delay(1500);
 }
